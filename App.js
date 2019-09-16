683ab7f6dee39802f603211ea6966fabe2b5c71e
SimoJS <mohamed.akraich@gmail.com>
2019-09-16 10:13:21 +0100

    Setting up redux and the ProductsOverviewScreen

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
+import { createStore, combineReducers } from "redux";
+import { Provider } from "react-redux";
+
+import productsReducer from "./store/reducers/products";
+import { create } from "uuid-js";
+
+const reducer = combineReducers({
+  products: productsReducer
+});
+
+const store = createStore(reducer);
+
 export default function App() {
   return (
-    <View style={styles.screen}>
-      <Text>The Shop App!</Text>
-    </View>
+    <Provider store={store}>
+      <View style={styles.screen}>
+        <Text>The Shop App!</Text>
+      </View>
+    </Provider>
   );
 }
 
---
+++ screens/ProductsOverviewScreen.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { View, Text, FlatList, StyleSheet } from "react-native";
+
+const ProductsOverviewScreen = props => {
+  return <FlatList />;
+};
+
+const styles = StyleSheet.create({});
+
+export default ProductsOverviewScreen;
